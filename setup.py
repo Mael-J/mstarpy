39433d1a0f48692999f1d689bb1d880daa77d9bf
Mael Jourdain <mael.jourdain@bsb-education.com>
2023-02-11 23:11:20 +0100

    version 0.0.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requirements(filename):
 setup(
     name="mstarpy",
     packages=find_packages(),
-    version="0.0.5",
+    version="0.0.6",
     license='MIT License',
     author="Maël Jourdain",
     author_email='mael.jourdain@gmail.com',
