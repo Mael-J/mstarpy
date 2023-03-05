721c55d7db2e4aebc1c986abd71d8a0a710b1585
Mael Jourdain <mael.jourdain@bsb-education.com>
2023-03-05 10:18:32 +0100

    sphinx v 5.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ release = '1.0.0'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '6.1.3'
+needs_sphinx = '5.3.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
