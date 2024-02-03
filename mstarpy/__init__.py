c89a9d0fce92c4b04e30fd329bcfbfdce4aceb85
Maël <mael.jourdain@gmail.com>
2024-02-03 18:30:47 +0100

    version 1.0.4

--- mstarpy/__init__.py
+++ mstarpy/__init__.py
@@ -2,4 +2,4 @@ from .funds import Funds
 from .stock import Stock
 from .search import filter_universe, search_field, search_filter, search_funds, search_stock
 
-__version__ = "1.0.5"
+__version__ = "1.0.4"
