f968ddd593aa51b92aec113c22a29134000f71c2
Maël <mael.jourdain@gmail.com>
2024-02-03 19:26:21 +0100

    pylint error.py

--- mstarpy/error.py
+++ mstarpy/error.py
@@ -4,22 +4,16 @@ def not_200_response(url,response):
 
     """
     if not response.status_code == 200:
-        raise ConnectionError(f"Error {response.status_code} for the api {url}. Message : {response.reason}.")
+        raise ConnectionError(f"""Error {response.status_code} 
+                              for the api {url}. Message : {response.reason}.""")
 
 
 def no_site_error(code, name, country, site):
     """ 
     This function raise a ValueError if the selected country is "us" or a site is not selected.
-    
     """
-    
         
     if not site or country == 'us':
         if country:
             raise ValueError(f"The funds of the country {country} cannot be scraped.")
-        else:
-            raise ValueError(f"The funds {name} ({code}) cannot be scraped.")
-
-
-
-    
+        raise ValueError(f"The funds {name} ({code}) cannot be scraped.")
