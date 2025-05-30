ea065760e10838b886d809856f26338dba44e094
Maël <mael.jourdain@gmail.com>
2025-05-30 15:44:09 +0200

    version 3.0.0

--- mstarpy/__init__.py
+++ mstarpy/__init__.py
@@ -5,4 +5,4 @@ from .funds import Funds
 from .stock import Stock
 from .search import filter_universe, search_field, search_filter, search_funds, search_stock
 
-__version__ = "2.0.2"
+__version__ = "3.0.0"
