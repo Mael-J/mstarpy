479f2e0f0803a7e244e0f14bef8af40d20486871
Maël <mael_jourdain@myria-am.com>
2026-03-15 11:29:22 +0100

    version 9.0.1

--- mstarpy/__init__.py
+++ mstarpy/__init__.py
@@ -4,4 +4,4 @@ from .funds import Funds
 from .stock import Stock
 from .search import MorningstarSession
 
-__version__ = "9.0.0"
+__version__ = "9.0.1"
--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requirements(filename):
 setup(
     name="mstarpy",
     packages=find_packages(),
-    version="9.0.0",
+    version="9.0.1",
     python_requires='>=3.10',
     license='MIT License',
     author="Maël Jourdain",
