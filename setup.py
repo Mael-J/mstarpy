ab84e0e2d5b5c8eb04c0ec5789c8852487abfdf5
Maël <mael_jourdain@myria-am.com>
2026-03-23 09:48:38 +0100

    version 9.0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def requirements(filename):
 setup(
     name="mstarpy",
     packages=find_packages(),
-    version="9.0.1+se_webdriver",
+    version="9.0.2",
     python_requires='>=3.10',
     license='MIT License',
     author="Maël Jourdain",
